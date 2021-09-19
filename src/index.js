22df15a8fc6df65d59cb0ea43e96213ea18ca0f1
x-thence <2668812066@qq.com>
2021-09-19 14:20:17 +0800

    feat: product模式下懒加载

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 //index.js
 import a from './a.js';
 import b from './b.js';
-import(/* webpackChunkName: 'ccc' */'./c')
+
+const obtn = document.createElement('button')
+    obtn.innerHTML = 'button'
+    document.body.appendChild(obtn)
+    obtn.addEventListener('click', function() {
+        // 引入自定的模块
+        import('./module').then(({ default: _ }) => {
+            console.log(_);
+        })
+    })
 function fn() {
     console.log('index-------');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,38 +8,38 @@ module.exports = {
     index: './src/index.js',
     // a: './src/a.js'
   },
-  mode: 'development',
+  mode: 'production', // production
   output: {
     filename: '[name][contenthash:6].js',
     chunkFilename: 'chunk_[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   optimization: {
-    chunkIds: 'deterministic',
+    // chunkIds: 'deterministic',
     runtimeChunk: true, // 提升性能，当只有被改变的模块发生变更时只会重新打包那个文件，浏览器直接从缓存中加载
     minimizer: [
       new TerserPlugin({
         extractComments: false // 去除production模式下打包生成的lisence文件
       })
     ],
-    splitChunks: {
-      minSize: 50,
+    // splitChunks: {
+      // minSize: 50,
       // chunks: 'all',
-      cacheGroups: {
-        common: {
-          name: 'common',
-          chunks: 'initial',
-          minChunks: 1,
-          priority: -20
-        },
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'initial',
-          priority: -10
-        }
-      }
-    }
+      // cacheGroups: {
+      //   common: {
+      //     name: 'common',
+      //     chunks: 'initial',
+      //     minChunks: 1,
+      //     priority: -20
+      //   },
+      //   vendor: {
+      //     test: /[\\/]node_modules[\\/]/,
+      //     name: 'vendor',
+      //     chunks: 'initial',
+      //     priority: -10
+      //   }
+      // }
+    // }
   },
   plugins: [
     new HtmlWebpackPlugin(),
